e47a74ab26978834cf62450770535c9a3e779382
Akwesi Kusi <therealkusi@gmail.com>
2021-09-08 00:46:20 -0700

    module

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import sys  # sys is a module
+# write writes in the std error. To use stdrerr you need sys
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+exit(1)
