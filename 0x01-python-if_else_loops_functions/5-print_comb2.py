fcf71c29134f7d29353136058c6fad58133ce11c
Akwesi Kusi <therealkusi@gmail.com>
2021-09-08 01:17:49 -0700

    comb

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for digit1 in range(0, 10):
-    for digit2 in range(digit1 + 1, 10):
-        if digit1 == 8 and digit2 == 9:
-            print("{}{}".format(digit1, digit2))
+for number in range(0, 100):
+    if number == 99:
+            print("{}".format(number))
         else:
-            print("{}{}".format(digit1, digit2), end=", ")
+            print("{:02}".format(number), end=", ")
