7933f534947ee24c4c4470efdc5f196b74d94bfb
Akwesi Kusi <therealkusi@gmail.com>
2021-09-14 08:19:25 -0700

    correction

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is "":
-        tup = (len(sentence), None)
-        return tup
-    else:
-        tup = (len(sentence), sentence[0])
-        return tup
+    length = len(sentence)
+    if (length == 0):
+        return (0, None)
+    return (length, sentence[0])
